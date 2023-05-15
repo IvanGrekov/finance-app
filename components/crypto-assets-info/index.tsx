dfb89fac6c0c7a8e0309cd5714fe804b1b5e97b7
Ivan Grekov <ivan.grekov@windmill.digital>
2023-05-15 19:12:25 +0300

    SectionWrapper added

--- components/crypto-assets-info/index.tsx
+++ components/crypto-assets-info/index.tsx
@@ -1,28 +1,20 @@
 import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import { styled } from '@mui/material/styles';
 
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-}));
+import SectionWrapper from 'components/section-wrapper';
 
 export default function CryptoAssetsInfo(): JSX.Element {
     return (
         <Grid container={true} spacing={2} p={2}>
             <Grid item={true} xs={6}>
-                <Item>xs=6</Item>
+                <SectionWrapper>xs=6</SectionWrapper>
             </Grid>
 
             <Grid item={true} xs={6}>
-                <Item>xs=6</Item>
+                <SectionWrapper>xs=6</SectionWrapper>
             </Grid>
 
             <Grid item={true} xs={12}>
-                <Item>xs=12</Item>
+                <SectionWrapper>xs=12</SectionWrapper>
             </Grid>
         </Grid>
     );
---
+++ components/section-wrapper/index.tsx
@@ -0,0 +1,11 @@
+import { PropsWithChildren } from 'react';
+
+import Paper from '@mui/material/Paper';
+
+export default function SectionWrapper({ children }: PropsWithChildren): JSX.Element {
+    return (
+        <Paper variant="outlined" sx={{ p: 2, borderRadius: 2 }}>
+            {children}
+        </Paper>
+    );
+}
