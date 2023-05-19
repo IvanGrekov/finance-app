7024b3337e31d8c68c85af57e634812dfb48e1b5
Ivan Grekov <ivan.grekov@windmill.digital>
2023-05-19 23:08:37 +0300

    mode for char fixed

--- components/assets-profit-section/Chart.tsx
+++ components/assets-profit-section/Chart.tsx
@@ -1,21 +1,38 @@
 import ApexChart from 'react-apexcharts';
 
-const options = {
-    chart: {
-        id: 'basic-bar',
-    },
-    xaxis: {
-        categories: ['13 May', '14 May', '15 May', '16 May', '17 May', '18 May', '19 May'],
-    },
-};
-
-const series = [
-    {
-        name: 'series-1',
-        data: [950, 1120, 1340, 850, 769, 1760, 2430, 2750],
-    },
-];
+import Box from '@mui/material/Box';
 
 export default function Chart(): JSX.Element {
-    return <ApexChart options={options} series={series} type="bar" />;
+    return (
+        <Box>
+            <ApexChart
+                options={{
+                    chart: {
+                        id: 'basic-bar',
+                    },
+                    xaxis: {
+                        categories: [
+                            '13 May',
+                            '14 May',
+                            '15 May',
+                            '16 May',
+                            '17 May',
+                            '18 May',
+                            '19 May',
+                        ],
+                    },
+                    theme: {
+                        mode: 'dark',
+                    },
+                }}
+                series={[
+                    {
+                        name: 'series-1',
+                        data: [950, 1120, 1340, 850, 1760, 2430, 2750],
+                    },
+                ]}
+                type="bar"
+            />
+        </Box>
+    );
 }
