bc9030ef7cc234c458b72a3341edb97f0ac089fd
Ivan Grekov <ivan.grekov@windmill.digital>
2023-05-19 23:23:16 +0300

    history section fixed

--- components/assets-history-section/index.tsx
+++ components/assets-history-section/index.tsx
@@ -1,5 +1,13 @@
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+
 import CacheBalanceHistory from 'components/cache-balance-history';
 
 export default function AssetsHistorySection(): JSX.Element {
-    return <CacheBalanceHistory />;
+    return (
+        <Stack spacing={2}>
+            <Typography>History</Typography>
+            <CacheBalanceHistory />
+        </Stack>
+    );
 }
