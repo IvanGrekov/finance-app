54904177cb674cda06a3eb057b11e5d551fa99f4
Ivan Grekov <ivan.grekov@windmill.digital>
2023-05-19 23:19:09 +0300

    mocked data for history fixed

--- components/cache-balance-history/constants/index.ts
+++ components/cache-balance-history/constants/index.ts
@@ -2,14 +2,46 @@ import { TCacheHistory } from 'models/types/history';
 import { ETransactionType } from 'models/types/transaction';
 
 export const MOCKED_CACHE_HISTORY: TCacheHistory = [
-    { id: '1', amount: 100, type: ETransactionType.DEPOSIT, balance: 100, date: new Date() },
-    { id: '2', amount: 200, type: ETransactionType.DEPOSIT, balance: 300, date: new Date() },
-    { id: '3', amount: 50, type: ETransactionType.WITHDRAW, balance: 250, date: new Date() },
-    { id: '4', amount: 100, type: ETransactionType.DEPOSIT, balance: 350, date: new Date() },
-    { id: '5', amount: 200, type: ETransactionType.WITHDRAW, balance: 150, date: new Date() },
-    { id: '6', amount: 300, type: ETransactionType.DEPOSIT, balance: 450, date: new Date() },
-    { id: '7', amount: 100, type: ETransactionType.WITHDRAW, balance: 350, date: new Date() },
-    { id: '8', amount: 200, type: ETransactionType.DEPOSIT, balance: 550, date: new Date() },
-    { id: '9', amount: 100, type: ETransactionType.WITHDRAW, balance: 450, date: new Date() },
-    { id: '10', amount: 200, type: ETransactionType.DEPOSIT, balance: 650, date: new Date() },
+    {
+        id: '1',
+        amount: 170,
+        type: ETransactionType.DEPOSIT,
+        balance: 100,
+        date: new Date(2023, 4, 14),
+    },
+    {
+        id: '2',
+        amount: 220,
+        type: ETransactionType.DEPOSIT,
+        balance: 300,
+        date: new Date(2023, 4, 15),
+    },
+    {
+        id: '3',
+        amount: 490,
+        type: ETransactionType.WITHDRAW,
+        balance: 250,
+        date: new Date(2023, 4, 16),
+    },
+    {
+        id: '4',
+        amount: 910,
+        type: ETransactionType.DEPOSIT,
+        balance: 350,
+        date: new Date(2023, 4, 17),
+    },
+    {
+        id: '5',
+        amount: 670,
+        type: ETransactionType.DEPOSIT,
+        balance: 150,
+        date: new Date(2023, 4, 18),
+    },
+    {
+        id: '6',
+        amount: 320,
+        type: ETransactionType.DEPOSIT,
+        balance: 450,
+        date: new Date(2023, 4, 19),
+    },
 ];
